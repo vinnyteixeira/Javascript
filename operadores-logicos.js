319e291ad54f135176daad49129d5231f09f7a10
Vinny Teixeira <87780451+vinnyteixeira@users.noreply.github.com>
2022-03-24 14:16:09 -0300

    Delete operadores-logicos.js

--- operadores-logicos.js
+++
@@ -1,8 +0,0 @@
-const idadeNadador= 5;
-
-if (idadeNadador >= 5 && idadeNadador <= 7)  console.log("infantil A");
-else if (idadeNadador >= 8 && idadeNadador <= 11) console.log("infantil B");
-else if (idadeNadador >= 12 && idadeNadador <= 13) console.log("juvenil A");
-else if (idadeNadador >= 14 && idadeNadador <= 17) console.log("juvenil B");
-else if (idadeNadador >= 18) console.log("Adulto");
-else console.log ("Não tem idade o suficiente");
